a0f7bfa5c6e1cd5ccf9712f0763ec4f4ea42896d
Luis David Cardona Calderon <luis.cardona@globant.com>
2022-07-08 11:00:14 -0500

    bugfix

--- controllers/bookings.controller.js
+++ controllers/bookings.controller.js
@@ -63,7 +63,6 @@ const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
   const price = session.amount_total / 100;
-  console.log(tour, user, price);
   await Booking.create({ tour, user, price });
 };
 
