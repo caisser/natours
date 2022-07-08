66e4cd323fbf62322e511d38025b33033196d06d
Luis David Cardona Calderon <luis.cardona@globant.com>
2022-07-08 10:14:29 -0500

    Bug fix

--- controllers/bookings.controller.js
+++ controllers/bookings.controller.js
@@ -68,7 +68,7 @@ exports.webhookCheckout = (req, res, next) => {
   const signature = req.headers['stripe-signature'];
   let event;
   try {
-    event = stripe.webHooks.constructEvent(
+    event = stripe.webhooks.constructEvent(
       req.body,
       signature,
       process.env.STRIPE_WEBHOOK_SECRET
